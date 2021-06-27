90e34979894d21c632354b400f35d5d33a8533de
KAVYA29-NITTE123 <86519455+KAVYA29-NITTE123@users.noreply.github.com>
2021-06-27 21:08:33 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,75 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+const PORT = process.env.PORT || 3300
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')
+const passport = require('passport')
 
+
+mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, 
+    useFindAndModify : true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Connection failed...')
+});
+
+
+
+
+//let mongoStore = new MongoDbStore({
+   //mongooseConnection: connection,
+   //collection: 'sessions'
+  //})
+
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: MongoDbStore.create({
+        mongoUrl: process.env.MONGO_CONNECTION_URL
+
+    }),
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
+}))
+
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    res.locals.user = req.user
+    next()
+})
+       
+   
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+
+require('./routes/web')(app) 
+
+
+
+
+app.listen(PORT , () => {
+    console.log(`Listening on port ${PORT}`) 
+}
+)
